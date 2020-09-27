643c5b9750c06867a1fdea327b2e27de6ba351ae
slyo <sean.lyo@outlook.com>
2020-09-27 18:12:03 +0800

    builtin: writing files with io and bufio

---
+++ builtin_writing_file/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+
+	d1 := []byte("hello\ngo\n")
+	err := ioutil.WriteFile("/tmp/dat1", d1, 0644)
+	check(err)
+
+	f, err := os.Create("/tmp/dat2")
+	check(err)
+
+	defer f.Close()
+
+	d2 := []byte{115, 111, 109, 101, 10}
+	n2, err := f.Write(d2)
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n2)
+
+	n3, err := f.WriteString("writes\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n3)
+
+	f.Sync()
+
+	w := bufio.NewWriter(f)
+	n4, err := w.WriteString("buffered\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n4)
+
+	w.Flush()
+
+	f2, err := os.OpenFile("/tmp/dat2", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	check(err)
+
+	n5, err := f2.WriteString("append mode\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n5)
+}
